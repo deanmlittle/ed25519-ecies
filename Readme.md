b7e26b0405c86907e89343e9d2889fcad6c54597
aresastro <dean@bitping.com>
2023-10-16 04:05:46 +0800

    Added tests and reaedme

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Ed25519 ECIES
+
+SIE1: A BIE1-based ECIES implementation that is compatible with Ed25519 keys on Solana.
+
+
+### Encryption
+The encryption protocol does the following:
+
+1) Converts an Ed25519 public and private key to their X25519 keys. If no private key is provided, a new one is generated on the fly.
+2) Computes a shared secret using ECDH
+3) Creates a Public key from the shared secret
+4) SHA512 hashes it and splits the resulting bytes into iv, kE and kM
+5) AESCBC encrypts the desired message with kE and iv
+6) Checksums the message with kM using SHA256-HMAC
+
+### Decryption
+The decryption protocol reverses this by:
+
+1) Takes in the private key of the opposing public key provided in the encryption phase
+2) Extracts the public of the opposing private key provided/created in the encryption phase
+3) Computes the same shared secret using ECDH, but with the opposing keys to those in the encryption phase
+5) Creates a Public key from the shared secret
+6) SHA512 hashes it and splits the resulting bytes into iv, kE and kM
+7) AESCBC decrypts the ciphertext with kE and iv
+8) Calculates the SHA256-HMAC of the message to ensure the provided checksum matches
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,16 +2,16 @@ import { Ed25519Ecies } from "../src"
 import { sign } from 'tweetnacl';
 import { assert } from "chai";
 
-describe("My Test Suite", () => {
+describe("ECIES encryption tests", () => {
+  // Set some Ed25519 key pairs for Alice and Bob
+  const aliceSecret = Buffer.from("8099218df05be91769679587124cfb3c1f6b0602805ffda193f26790c531e1eb", "hex");
+  const bobSecret = Buffer.from("8ed0ce08849ef03657e0f137f15b73afbfc4ecbfcc76505e5fb5f63c998bb8a0", "hex");
+
+  // Create keypairs
+  const aliceEd25519KeyPair = sign.keyPair.fromSeed(aliceSecret);
+  const bobEd25519KeyPair = sign.keyPair.fromSeed(bobSecret);
+
   it("E2E checks for encrypt/descrypt", async () => {
-    // Set some Ed25519 key pairs for Alice and Bob
-    const aliceSecret = Buffer.from("8099218df05be91769679587124cfb3c1f6b0602805ffda193f26790c531e1eb", "hex");
-    const bobSecret = Buffer.from("8ed0ce08849ef03657e0f137f15b73afbfc4ecbfcc76505e5fb5f63c998bb8a0", "hex");
-
-    // Create keypairs
-    const aliceEd25519KeyPair = sign.keyPair.fromSeed(aliceSecret);
-    const bobEd25519KeyPair = sign.keyPair.fromSeed(bobSecret);
-
     // Encrypt and decrypt message
     const messageEncrypted = await Ed25519Ecies.encrypt(Buffer.from("Hello!"), aliceEd25519KeyPair.publicKey, bobEd25519KeyPair.secretKey);
     const messageEncrypted2 = await Ed25519Ecies.encrypt(Buffer.from("Hello!"), bobEd25519KeyPair.publicKey, aliceEd25519KeyPair.secretKey);
@@ -19,6 +19,17 @@ describe("My Test Suite", () => {
     assert(Buffer.from(messageEncrypted.subarray(36, messageEncrypted.length-32)).equals(messageEncrypted2.subarray(36, messageEncrypted2.length-32)));
     // Make sure they do not equal the unencrypted string
     assert(Buffer.from(messageEncrypted.subarray(36, messageEncrypted.length-32)).toString() !== "Hello!")
+    const messageDecrypted = await Ed25519Ecies.decrypt(messageEncrypted, aliceEd25519KeyPair.secretKey);
+    assert(Buffer.from(messageDecrypted).toString() === "Hello!")
+  });
+
+  it("Testing with ephemeral private key", async () => {
+    // Create keypairs
+    const aliceEd25519KeyPair = sign.keyPair.fromSeed(aliceSecret);
+    const bobEd25519KeyPair = sign.keyPair.fromSeed(bobSecret);
+
+    // Encrypt and decrypt message
+    const messageEncrypted = await Ed25519Ecies.encrypt(Buffer.from("Hello!"), aliceEd25519KeyPair.publicKey);
     const messageDecrypted = await Ed25519Ecies.decrypt(messageEncrypted, aliceEd25519KeyPair.secretKey);
     assert(Buffer.from(messageDecrypted).toString() === "Hello!")
   });
